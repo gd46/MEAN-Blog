fdef4cf82679c55c4487e3936d65ade8ba3eca13
Giuseppe DiBella <gd46@njit.edu>
2015-07-12 13:48:00 -0400

    Started to create new api routes

--- app/routes/api.js
+++ app/routes/api.js
@@ -4,13 +4,38 @@ var bodyParser = require('body-parser'),
 
 
 module.exports = function(app, express){
-  var apiRouter = express.Router()l
+  var apiRouter = express.Router();
+
+  // apiRouter.use(function(req,res,next){
+  //   console.log('Somebody just came to our app');
+  //   next();
+  // });
 
   apiRouter.get('/', function(req, res){
     res.json({
       message: 'Welcome to api'
     });
   });
+
+  apiRouter.route('/blog')
+    .post(function(req,res){
+      var post = new BlogPost();
+      post.title = req.body.title;
+      post.caption = req.body.caption;
+      post.description = req.body.description;
+
+      post.save(function(err){
+        if(err){
+          return res.json({
+            success: false,
+            message: 'Failed to create blog post, error status ' + err
+          });
+        }
+        res.json({
+          message: 'Sucessfully created blog post'
+        })
+      });
+    });
 
   return apiRouter;
 }
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express'),
-  app = express();
-  bodyParser = requir('body-parser'),
+  app = express(),
+  bodyParser = require('body-parser'),
   morgan = require('morgan'),
   path = require('path'),
+  mongoose = require('mongoose'),
   config = require('./config.js');
 
 
-mogoose.connect(config.database);
+mongoose.connect(config.database);
 
 mongoose.connection.on("connected", function(){
   console.log("Successfully connected to DB");
@@ -19,6 +20,7 @@ mongoose.connection.on("error", function(err){
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(bodyParser.json());
 
 app.use(function(req,res,next){
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -31,12 +33,12 @@ app.use(morgan('dev'));
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('*', function(req,res){
-  res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
-});
-
 var apiRoutes = require('./app/routes/api.js')(app, express);
 app.use('/api', apiRoutes);
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
+});
 
 app.listen(config.port);
 console.log("Magic happens on port " + config.port);
