729e3401e7fe6fe91dafa99b2bcbc20106a51d1c
Giuseppe DiBella <gd46@njit.edu>
2015-07-13 20:41:50 -0400

    Added new routes to handle CRUD for blog posts

--- app/routes/api.js
+++ app/routes/api.js
@@ -18,6 +18,8 @@ module.exports = function(app, express){
   });
 
   apiRouter.route('/blog')
+
+    // Create a blog post
     .post(function(req,res){
       var post = new BlogPost();
       post.title = req.body.title;
@@ -34,6 +36,70 @@ module.exports = function(app, express){
         res.json({
           message: 'Sucessfully created blog post'
         })
+      });
+    })
+
+    // Grab all blog posts
+    .get(function(req, res){
+      BlogPost.find(function(err, posts){
+        if(err){
+          res.send(err);
+        } else {
+          res.json(posts);
+        }
+      });
+    });
+
+
+  // Individual blog post routes
+  apiRouter.route('/blog/:blog_id')
+    .get(function(req, res){
+      BlogPost.findById(req.params.blog_id, function(err, post){
+        if(err) {
+          res.send(err);
+        } else{
+          res.json(post);
+        }
+      });
+    })
+
+    .put(function(req, res){
+      BlogPost.findById(req.params.blog_id, function(err, post){
+        if(err){
+          res.send(err);
+        }
+
+        if(req.body.title){
+          post.title = req.body.title;
+        }
+        if(req.body.caption){
+          post.caption = req.body.caption;
+        }
+        if(req.body.description){
+          post.description = req.body.description;
+        }
+        post.save(function(err){
+          if(err){
+            res.send(err);
+          } else{
+            res.json({
+              success: true,
+              message: 'Successfully updated blog post'
+            })
+          }
+        })
+      })
+    })
+
+    .delete(function(req, res){
+      BlogPost.remove({
+        _id: req.params.blog_id
+      }, function(err, post){
+        if(err) return res.send(err);
+        res.json({
+          success: true,
+          message: 'Successfully Deleted Blog Post'
+        });
       });
     });
 
